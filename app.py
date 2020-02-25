8d7887e10579ac510f56dec320e7a824dddc56a6
Mario McGee <mario.mcgee@students.makeschool.com>
2020-02-25 13:43:40 -0800

    app.py route added

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def create_words():
-    my_file= open("words.txt",'r')
-    lines = myfile.readlines()
-    my_histogram = histogram(lines)
+   # my_file= open("words.txt",'r')
+   # lines = my_file.readlines()
+   # print(lines)
+    my_histogram = histogram('words.txt')
 
     word = sample(my_histogram)
+    print(word)
     return word
 
--- sample.py
+++ sample.py
@@ -4,21 +4,21 @@ from histogram import histogram
 txt = [('cats', 3), ('dog', 4), ('rabbits', 2), ('turtles', 1)]
 
 
-
-
+# histogram1 = {'hello':1, 'bye':2}
+# print(histogram1.items())
 def sample(histogram):
-   
-    tokens =  sum([count for word, count in histogram]) 
+    print(histogram)
+    tokens =  sum([count for word, count in histogram.items()]) 
     
     dart = random.randint(1,tokens)
     fence = 0
 
-    for word, count in histogram:
+    for word, count in histogram.items():
         fence += count
         if fence >= dart:
          return word
 
-print(sample(txt)) 
+
 
     
 
